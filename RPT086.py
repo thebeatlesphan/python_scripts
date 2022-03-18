260203adc92a3098c76f32a8fd091190081e9819
andy <aphanIT@protonmail.com>
2022-03-17 21:56:01 -0400

    completed RPT086 resort program

--- RPT086.py
+++ RPT086.py
@@ -1,20 +1,53 @@
-#GUI module
-import tkinter as tk
+import tkinter as tkinter
 from tkinter import filedialog
+import re
 
-#start GUI library
 root = tk.Tk()
-#hide root window
 root.withdraw()
-#show file dialog
 file_path = filedialog.askopenfilename()
 
-new_sort = []
-with open(file_path,"a") as f:
-	lines = readlines()
+int_sort = []
+str_sort = []
 
+with open(file_path) as f:
+	lines = f.readlines()
 	for line in lines:
-		temp = line.split("|")
-		new_sort.append(temp[0])
-		new_sort.append(temp[1])
+		if "|" in line:
+			temp = line.split(',')
+			for x in temp:
+				all_digits = re.match('^[\d]*,', x.strip())
+				if all_digits:
+					int_sort.apprend(x.strip())
+				elif not all_digits:
+					str_sort.append(x.strip())
 
+int_dict = {}
+int_keys = []
+str_dict = {}
+str_keys = []
+
+for x in int_sort:
+	temp = x.split(',')
+	if len(temp[0]) > 0:
+		int_dict[temp[0]] = 0
+		int_keys.append(int(temp[0]))
+for x in str_sort:
+	temp = x.split(',')
+	if len(temp[0]) > 0:
+		str_dict[temp[0]] = 0
+		str_keys.append(temp[0])
+
+str_keys.sort()
+int_keys.sort()
+for x in range(0, len(int_keys)):
+	int_keys[x] = str(int_keys[x])
+
+new_file = input("What would you like to name your file?\n")
+file_name = new_file + ".csv"
+
+f = open(file_name, 'w')
+for x in str_keys:
+	f.write(str_dict[x] + '\n')
+for x in int_keys:
+	f.write(int_dict[x] = '\n')
+f.close()
