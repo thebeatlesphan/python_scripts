ccfce52816271fd90ce0573ed8ae5149e71c1009
andy <aphanIT@protonmail.com>
2022-04-19 23:46:08 -0400

    working with Urls

--- iv_addresses.py
+++ iv_addresses.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.tutorialspoint.com/index.htm"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-print(soup.title)
-print()
-for link in soup.find_all('a'):
-	print(link.get('href'))
+with open("yellow.html") as fp:
+	soup = BeautifulSoup(fp, "html.parser")
+
+	print(soup.prettify())
