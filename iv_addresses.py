5d1a9a6bd46cd69360eebfe5ebc96344b723f66b
andy <aphanIT@protonmail.com>
2022-04-19 23:26:36 -0400

    bs4 practice

--- iv_addresses.py
+++ iv_addresses.py
@@ -1,5 +1,10 @@
-from googlesearch import search
-for i in search("cat"):
-	print(i)
+from bs4 import BeautifulSoup
+import requests
 
-print("yeahhh")
+url = "https://www.tutorialspoint.com/index.htm"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+print(soup.title)
+print()
+for link in soup.find_all('a'):
+	print(link.get('href'))
