21cabb235893fadbd81b250933009be5713803c4
andy <aphanIT@protonmail.com>
2022-04-21 23:26:42 -0400

    beginning work on printer inc analysis with matplotlib

---
+++ datascience.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+file = ""
+df = pd.read_excel(file)
+df.plot(kind="", etc)
+
+
+#testing
+data = []
+for i in range(x,y):
+	print(i)
