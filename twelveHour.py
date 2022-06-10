90c1e470beb65d2b6f80618a604ebec23d4dfc48
andy <aphanIT@protonmail.com>
2022-06-10 16:10:52 -0400

    Completed twelveHour!!

--- twelveHour.py
+++ twelveHour.py
@@ -5,6 +5,13 @@
 # Compile the program to an exe via auto-python-to-exe library
 
 '''
+June 10th, 2022
+- Completed incorporating an INI file for group_names
+- Completed new UI
+
+May 21st, 2022
+- Implemented Treeview and configparser
+
 May 7th, 2022
 - Removed "Justin Quesada" and "Fernando Robles" from group variable
 - Added condition to format date "Created" column to change font color if >= 25 days ago
@@ -17,11 +24,27 @@ import warnings
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from datetime import date, timedelta
+import configparser
 
 #Global variables
 filepath = ""
 df = ""
 today = str(date.today() - timedelta(25))
+config_file = ""
+group_names = []
+
+if exists(file_path):
+	config_file = configparser.ConfigParser()
+	config_file.read(file_path)
+	for key in config_file["Name"]:
+		group_names.append(config_file["Name"][key])
+
+else:
+	os.mkdir(os.path.expanduser('~/Documents/twelveHour'))
+	config_file = configparser.ConfigParser()
+	config_file.add_section("Name")
+	with open(file_path, 'w') as config:
+		config_file.write(config)
 
 def open_file():
 	global df
@@ -43,6 +66,9 @@ def open_file():
 		cols = [cols[i] for i in myorder]
 		df = desired_team[cols].sort_values(by=["Assigned to"], ascending=True)
 		df["Created"] = df["Created"].dt.strftime('%Y-%m-%d')
+
+	status_indicator.configure(background="yellow")
+	status_indicator["text"] = 'File Loaded'
 
 def save_file():
 	global df
@@ -70,26 +96,66 @@ def save_file():
 		writer.sheets["andy"].set_column("F:F", 10, format)
 		writer.sheets["andy"].set_column("G:G", 40, format)
 		
-	file["text"] = 'DONE'
+	status_indicator.configure(background="green")
+	status_indicator["text"] = 'File Saved'
 
-#GUI for program
+def add_name():
+	global group_names
+
+	group_names.append(add_name_entry.get())
+	tree.insert('', tk.END, values=(add_name_entry.get(),))
+	config_file["Name"][add_name_entry.get()] = add_name_entry.get()
+	with open(file_path, 'w+') as config:
+		config_file.write(config)
+	add_name_entry.delete(0,100)
+
+def delete_name():
+	global group_names
+
+	selected_item = tree.selection()
+	for item in selected_item:
+		config_file.remove_option("Name", tree.item(item)['values'][0])
+		group_names.remove(tree.item[item]['values'][0])
+		tree.delete(item)
+	with open(file_path, 'w+') as config:
+		config_file.write(config)
+
+# GUI for program
 window = tk.Tk()
-window.title("12hour")
 
+window.title("Micheline's Little Helper")
+window.geometry('330x140')
 window.resizable(width=False, height=False)
-frame = tk.Frame(master=window, width=250, height=150)
-frame.pack()
+frame = tk.Frame(master=window)
 
-description = tk.Label(text="Micheline's Little Helper", font=8).place(x=20, y=20)
-
-btn_open = tk.Button(master=frame, text="Open", command=open_file).place(x=70, y=70)
+btn_open = tk.Button(master=frame, text="Open", command=open_file)
 btn_save = tk.Button(master=frame, text="Save As...", command=save_file)
-btn_save.place(x=120, y=70)
 
-text = tk.Entry(width=39)
-text.place(x=4, y=110)
+status = tk.Label(window, text="File Status").grid(row=0,column=0)
+status_indicator = tk.Label(window, text="No File Loaded").grid(row=1,column=0,rowspan=2)
 
-file = tk.Label(master=frame, text=filepath, wraplength=250)
-file.place(x=0, y=130)
+add_name_entry = tk.Entry(window).grid(row=4,column=0)
 
+btn_add = tk.Button(window, text="Add Agent", command=add_name).grid(row=5,column=0)
+btn_delete = tk.Button(window, text="Delete Agent", command=delete_name).grid(row=5, column=1)
+
+# Employee Treeview List
+tree = ttk.Treeview(window, height=4, columns=("Name"), show='headings')
+tree.grid(row=0, column=1, rowspan=5, sticky=tk.E)
+tree.heading("Name", text="Name")
+
+# Dropdown Menu
+menubar = Menu(window)
+window.config(menu=menubar)
+file_menu = Menu(menubar, tearoff=False)
+file_menu.add_command(label='Open', command=open_file)
+file_menu.add_command(label='Save as', command=save_file)
+file_menu.add_command(label='Exit', command=window.destroy)
+menubar.add_cascade(label="File", menu=file_menu)
+
+def update_tree():
+	for name in group_names:
+		tree.insert('', tk.END, values=(name,))
+
+update_tree()
 window.mainloop()
